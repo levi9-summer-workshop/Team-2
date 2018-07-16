43857f4597af16d48ae8c7e4f97b933d69fd788b
Koricanac023 <java@JAVAKURSZR04.levi9.com>
2018-07-16 14:27:10 +0200

    Create domain

---
+++ src/main/java/rs/levi9/survey/domain/Answer.java
@@ -0,0 +1,58 @@
+package rs.levi9.survey.domain;
+
+import javax.persistence.*;
+import javax.validation.constraints.NotNull;
+import java.io.Serializable;
+
+@Entity
+@Table(name = "answer")
+public class Answer extends BaseEntity implements Serializable {
+
+    @NotNull
+    @ManyToOne
+    @JoinColumn(name = "question_id", nullable = false)
+    private Question question;
+
+    @NotNull
+    @ManyToOne
+    @JoinColumn(name = "survey_answer_id", nullable = false)
+    private SurveyAnswer surveyAnswer;
+
+    @NotNull
+    @ManyToOne
+    @JoinColumn(name = "user_id", nullable = false)
+    private User user;
+
+    public Answer() {
+    }
+
+    public Answer(Question question, SurveyAnswer surveyAnswer, User user) {
+        this.question = question;
+        this.surveyAnswer = surveyAnswer;
+        this.user = user;
+    }
+
+    public Question getQuestion() {
+        return question;
+    }
+
+    public void setQuestion(Question question) {
+        this.question = question;
+    }
+
+    public SurveyAnswer getSurveyAnswer() {
+        return surveyAnswer;
+    }
+
+    public void setSurveyAnswer(SurveyAnswer surveyAnswer) {
+        this.surveyAnswer = surveyAnswer;
+    }
+
+    public User getUser() {
+        return user;
+    }
+
+    public void setUser(User user) {
+        this.user = user;
+    }
+}
---
+++ src/main/java/rs/levi9/survey/domain/Response.java
@@ -0,0 +1,4 @@
+package rs.levi9.survey.domain;
+
+public class Response {
+}
