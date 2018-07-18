7ef43b33f514e8993f525c174506be90db487587
halucinacija <vikicalov@gmail.com>
2018-07-18 15:35:47 +0200

    Front home page

--- front/src/app/app.module.ts
+++ front/src/app/app.module.ts
@@ -10,15 +10,12 @@ import { HeaderComponent } from './header/header.component';
 import { FooterComponent } from './footer/footer.component';
 import { HomeComponent } from './home/home.component';
 import { MyServeysComponent } from './my-serveys/my-serveys.component';
-<<<<<<< HEAD
 import { HttpClientModule } from '@angular/common/http';
 import { UserService } from './registration/user.service';
-=======
 import { SharedComponent } from './shared/shared.component';
 import { ConfirmEqualValidatorDirective } from './shared/confirm-equal-validator.directive';
 
 
->>>>>>> 8d28e13f4846eb117991ad803d2a965e01b8c41e
 
 @NgModule({
   declarations: [
@@ -28,14 +25,10 @@ import { ConfirmEqualValidatorDirective } from './shared/confirm-equal-validator
     HeaderComponent,
     FooterComponent,
     HomeComponent,
-<<<<<<< HEAD
     MyServeysComponent,
-=======
-    UsersComponent,
     MyServeysComponent,
     SharedComponent,
     ConfirmEqualValidatorDirective
->>>>>>> 8d28e13f4846eb117991ad803d2a965e01b8c41e
   ],
   imports: [
     BrowserModule,
